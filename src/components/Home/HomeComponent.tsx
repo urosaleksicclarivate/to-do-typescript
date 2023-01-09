790d22640c52728b4bbb037439d0c29169985da0
urosaleksicclarivate <uros.aleksic@clarivate.com>
2023-01-09 13:12:43 +0100

    Adding rendering logic to HomeComponent

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import AuthContext from "../../context/auth-context";
 import classes from "./HomeComponent.module.css";
 const HomeComponent: React.FC = () => {
+  const authCtx = useContext(AuthContext);
+
   return (
     <section className={classes.starting}>
-      <h1>Hello from home component!</h1>
+      {!authCtx.isLoggedIn && <h1>Welcome, log in to proceed!</h1>}
+      {authCtx.isLoggedIn && <h1>Welcome, go to tasks page!</h1>}
     </section>
   );
 };
